cd93d81634dd135af4b7231ca923b4ae38f822aa
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-05 15:48:21 -0300

    feat: POST method to request data from input to server

--- frontend/js/routers.js
+++ frontend/js/routers.js
@@ -1,6 +1,27 @@
-// fetch the localhost:8080/api/lembretes 
+// fetch the localhost:7070/api/lembrete
 // get the criar lembrete method
 
 // Path: frontend\js\routers.js
 
 
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', event => {
+    
+    event.preventDefault();
+
+    const formData = new FormData(formEl); 
+    const jsonObject = Object.fromEntries(formData);
+    //const jsonData = JSON.stringify(jsonObject);
+
+    fetch('http://localhost:7070/api/lembrete', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonObject), // body data type must match "Content-Type" header
+    })
+    .then(response => response.json())
+    .then(data => window.alert("Lembrete criado com sucesso!"))
+    .catch(error => console.log(error));
+});
